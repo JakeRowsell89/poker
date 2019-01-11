80ea7bf308eaffe51d3c1395f0f3783e2ee15e47
Dima Tsaruk <dzmitry.tsaruk@bglgroup.co.uk>
2019-01-11 17:55:08 +0300

    skeleton react app

---
+++ poker-front/src/Game.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+export default class Game extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+
+    }
+
+    redraw() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                HELLo WORLD
+            </div>
+        );
+    }
+
+}
